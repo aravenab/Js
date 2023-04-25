252f2b91fbd1a3aaf85626d92f3ccba47938e79f
Tomás Aravena <taravenab@uft.edu>
2023-04-25 10:23:37 -0400

    cambios para github y ayuda de coder

--- js/main.js
+++ js/main.js
@@ -22,6 +22,59 @@
 // Término de la primera entrega
 
 //Inicio de la segunda entrega ---------------------------------------------------------------
+// const ivaInfo = {
+//     tasa: 1.19,
+//     moneda: 'CLP'
+//   };
+  
+//   function calcular(monto, ivaInfo) {
+//     let iva = monto * ivaInfo.tasa;
+//     let total = monto + iva;
+//     return {
+//       monto: monto,
+//       iva: iva,
+//       total: total,
+//       moneda: ivaInfo.moneda
+//     };
+//   }
+  
+//   function buscarResultadoPorMonto(monto) {
+//     return resultados.find(resultado => resultado.monto === monto);
+//   }
+  
+//   let resultados = [];
+  
+//   for (let i = 0; i < 3; i++) {
+//     let mensaje = `Ingresa el monto número ${i + 1} para calcular el Bruto de todos tus montos ingresados juntos`;
+//     let monto = parseInt(prompt(mensaje));
+//     if (monto <= 0) {
+//       console.log("No se puede calcular el IVA de números negativos o de 0");
+//       continue;
+//     }
+//     let resultado = calcular(monto, ivaInfo);
+//     resultados.push(resultado);
+//   }
+  
+//   resultados = resultados.filter(resultado => resultado.monto > 0);
+  
+//   console.log(resultados);
+  
+//   let montoBuscado = parseInt(prompt("Ingrese el monto que desea buscar"));
+//   let resultadoBuscado = buscarResultadoPorMonto(montoBuscado);
+  
+//   if (resultadoBuscado) {
+//     console.log(`El resultado para el monto ${montoBuscado} es: ${JSON.stringify(resultadoBuscado)}`);
+//   } else {
+//     console.log(`No se encontró un resultado para el monto ${montoBuscado}`);
+//   }
+  
+//   let montoTotal = resultados.reduce((total, resultado) => total + resultado.total, 0);
+  
+//   console.log(`El monto total a pagar es: ${montoTotal} ${ivaInfo.moneda}`);
+// fin segunda entrega
+
+// inicio tercera entrega ----------------------------------------------------------------------------------
+
 const ivaInfo = {
     tasa: 1.19,
     moneda: 'CLP'
@@ -71,4 +124,23 @@ const ivaInfo = {
   let montoTotal = resultados.reduce((total, resultado) => total + resultado.total, 0);
   
   console.log(`El monto total a pagar es: ${montoTotal} ${ivaInfo.moneda}`);
+
+ 
+
+
+
+
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    // Selecionar el botón por su clase "btn"
+    const btn = document.querySelector('.btn');
+  
+    // Cambiar el texto del botón
+    btn.textContent = '¡Realizar el calculo!';
+  
+    // Agregar un listener al botón
+    btn.addEventListener('click', () => {
+      console.log('¡Haz clickeado el botón!');
+    });
+  });
   
